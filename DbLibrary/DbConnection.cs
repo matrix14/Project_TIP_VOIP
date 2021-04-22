08686aebed944724895a099e73482b11969864a7
matrix14 <modeft1999@gmail.com>
2021-04-22 18:54:16 +0200

    db_fix

--- DbLibrary/DbConnection.cs
+++ DbLibrary/DbConnection.cs
@@ -12,7 +12,7 @@ namespace DbLibrary
         public DbConnection()
         {
             server = "10.8.0.1";
-            database = "projekt_tip_kacper";
+            database = "projekt_kacper_tip";
             uid = "user_io";
             password = "DN8OHj8mUkNmXBRm";
             string connectionString;
